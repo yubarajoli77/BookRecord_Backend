d46a3d94ebbf5c7e372388d6c1c93f09075bb43a
mr011 <yubarajoli77@gmail.com>
2023-08-10 21:38:01 +0545

    feat: add user and book router and respective function to db.js

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
+const User = require('../model/user.js');
 
 const connect = ()=>{
   mongoose.connect(process.env.mongo).then(res=>{
@@ -7,4 +8,18 @@ const connect = ()=>{
   });
 }
 
-module.exports = {connect};
+const disconnect = ()=>{
+  mongoose.connection.close().then(res=>{
+    console.log('MongoDB is disconnected!!!');
+  });
+}
+
+const findUser = async (obj) =>{
+  return await User.findOne(obj);
+};
+
+const saveUser = async (user) =>{
+  return await user.save();
+};
+
+module.exports = {connect, disconnect, findUser, saveUser};
--- router/user.js
+++ router/user.js
@@ -1,62 +1,13 @@
-const express = require('express');
+const express = require('express'):
+
 const router = express.Router();
 
-router.get('/', (req,res,next)=>{
-  res.status(200).json({
-    message: "Success user get",
-    metadata: {
-      hostname: req.hostname,
-      method: req.method
-    }
-  });
-})
+router.post('/register', (req, res, next)=>{
 
-router.get("/:id",(req,res,next)=>{
-  res.status(200).json({
-    message: "Success user get by id",
-    metadata:{
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method
-    }
-  })
-})
+    
+});
 
-router.post('/', (req,res,next)=>{
-  const body = req.body;
-
-  res.status(200).json({
-    message: "Success user post",
-    metadata: {
-      body: body,
-      hostname: req.hostname,
-      method: req.method
-    }
-  });
-})
+router.post('/login', (req, res, next)=>{
 
 
-router.put('/:id', (req,res,next)=>{
-  res.status(200).json({
-    message: "Success user put",
-    metadata: {
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method
-    }
-  });
-})
-
-
-router.delete('/:id', (req,res,next)=>{
-  res.status(200).json({
-    message: "Success user delete",
-    metadata: {
-      id: req.params.id,
-      hostname: req.hostname,
-      method: req.method
-    }
-  });
-})
-
-module.exports = router;
+});
